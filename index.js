263edf59c506894a900e8f262ec77b28ba9756dd
Muhammad Roisul Adib <99470918+roisuladib@users.noreply.github.com>
2023-07-18 14:32:01 +0700

    change: port

--- index.js
+++ index.js
@@ -39,6 +39,6 @@ const server = http.createServer(async (req, res) => {
    }
 });
 
-server.listen(19000, () => {
-   console.log('Server berjalan di http://localhost:19000');
+server.listen(3000, () => {
+   console.log('Server berjalan di http://localhost:3000');
 });
